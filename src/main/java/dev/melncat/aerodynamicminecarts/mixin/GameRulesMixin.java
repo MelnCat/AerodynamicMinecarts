c74ec93f66ba826e5ff03b521797c507f7bd7f46
MelnCat <melncatuwu@gmail.com>
2024-08-19 16:47:42 -0700

    progress

--- src/main/java/dev/melncat/aerodynamicminecarts/mixin/GameRulesMixin.java
+++ src/main/java/dev/melncat/aerodynamicminecarts/mixin/GameRulesMixin.java
@@ -1,8 +1,17 @@
 package dev.melncat.aerodynamicminecarts.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.world.entity.vehicle.Minecart;
 import net.minecraft.world.level.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
-
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.points.BeforeFieldAccess;
 @Mixin(GameRules.class)
 abstract class GameRulesMixin {
+	@ModifyExpressionValue(method = "<clinit>", at = @At(value = "CONSTANT", args = "intValue=1000"))
+	private static int minecartLimit(int value) {
+		return Integer.MAX_VALUE;
+	}
 }
---
+++ src/main/java/dev/melncat/aerodynamicminecarts/mixin/NewMinecartBehaviorMixin.java
@@ -0,0 +1,17 @@
+package dev.melncat.aerodynamicminecarts.mixin;
+
+import net.minecraft.world.entity.vehicle.AbstractMinecart;
+import net.minecraft.world.entity.vehicle.NewMinecartBehavior;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(NewMinecartBehavior.class)
+abstract class NewMinecartBehaviorMixin {
+	@Inject(method = "getSlowdownFactor", at = @At("HEAD"), cancellable = true)
+	private void getSlowdownFactor(CallbackInfoReturnable<Double> cir) {
+		cir.setReturnValue(2d);
+	}
+}
